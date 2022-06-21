5afe5ba082b8915a4bf0ecdbacfbdbf976254324
JonathanArriagada <jonathan.arriagada@live.com>
2022-06-21 17:27:33 -0300

    icono carrito agregado

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,12 @@
+import './ItemListContainer.css';
+
+function ItemListContainer() {
+    return (
+      <div className="">
+         <h1>Lista de productos:</h1>
+         <p>¡ Aqui encontraras los mejores productos de electronica con los precios mas bajos !</p>
+      </div>
+    );
+  }
+
+  export default ItemListContainer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,22 @@
 import './Navbar.css'
+import carrito from './shopping-cart.png'
 
 function Navbar() {
     return (
-        <header>
-        <nav>
-          <ul>
-              <li class="botones"><a href="">Inicio</a></li>
-              <li class="botones"><a href="">Informacion</a></li>
-              <li class="botones"><a href="">Productos</a></li>
-              <li class="botones"><a href="">Contacto</a></li>
-          </ul>
-        </nav>
+      <header>
+        <div>
+          <nav>
+            <ul>
+              <a className="carrito">
+                <img width="50" src={carrito} alt="logo"/>
+              </a>  
+              <li className="botones"><a href="">Inicio</a></li>
+              <li className="botones"><a href="">Informacion</a></li>
+              <li className="botones"><a href="">Productos</a></li>
+              <li className="botones"><a href="">Contacto</a></li>
+            </ul>
+          </nav>
+        </div>
       </header>
     );
   }
